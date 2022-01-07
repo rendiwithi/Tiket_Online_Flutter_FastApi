3f84760c8619edfc5eb883531d84804df018ed7b
Rendi Dwi Hermawan <56640025+rendiwithi@users.noreply.github.com>
2022-01-07 10:56:21 +0700

    Update db.py

--- database_fastapi/config/db.py
+++ database_fastapi/config/db.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine, MetaData, engine
 engine = create_engine('mysql+pymysql://root:@localhost:3306/tiket_db')
 meta = MetaData()
 conn = engine.connect()
-ip_public = "http://192.168.0.104"
+ip_public = "http://47.91.56.36"
 storage = "/var/www/html/images/"
