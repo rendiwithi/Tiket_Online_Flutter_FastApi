9feb16ad8f89e4c6bf64e329ea0be366d007882a
rendiwithi <rendidwih2@gmail.com>
2022-01-07 11:18:49 +0700

    migrate

--- database_fastapi/config/db.py
+++ database_fastapi/config/db.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine, MetaData, engine
 engine = create_engine('mysql+pymysql://root:@localhost:3306/tiket_db')
 meta = MetaData()
 conn = engine.connect()
-ip_public = "http://47.91.56.36"
+ip_public = "https://krisnawahyu.my.id"
 storage = "/var/www/html/images/"
